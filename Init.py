1126bbb48204eba4bf896413f55bdac44bedf3bb
Marcos Gomes <marcosartemio221@gmail.com>
2023-07-04 22:32:56 -0300

    Screen
    Tela calculadora

--- Init.py
+++ Init.py
@@ -2,8 +2,12 @@ from tkinter import *
 
 class Main:
     def __init__(self,master):
-        self.containerBack = Label(master, background='RED', text='Vermelho')
-        self.containerBack.pack(fill=X)
+        self.containerDisplay = Frame(master, background='RED')
+        self.containerDisplay.pack(fill=X)
+        self.defaultFont = ('Verdana',13)
+
+        self.display = Entry(self.containerDisplay, justify='right', font=self.defaultFont)
+        self.display.pack()
 
 init = Tk()
 init.geometry('200x200')
