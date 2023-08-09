ac54e7ea329f5131b70e7ed3e553b0c082f655eb
Marcos Gomes <marcosartemio221@gmail.com>
2023-08-09 19:42:23 -0300

    Fixing Bugs
    Bug: Ao aperta a tecla 'c' o tkinter reconhece como sendo '-', criando uma exceção.

--- Functions.py
+++ Functions.py
@@ -16,6 +16,8 @@ def alterSignal(display:str): #Altera o sinal do número que está sendo mostrad
         return ''
 
 def operation(display:str,event,operator:str): #Seta a operação que vai ser feita
+    if event.char == 'c':
+        return display
     if operator == '':
         operator = event.char
     try:
